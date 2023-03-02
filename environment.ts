772e2b58834c30d33785598bcc6e95edc80ef388
khizer.ali <khizer.ali@khizar.millsoft.com.pk>
2023-03-02 15:15:29 +0500

    backend setup

---
+++ environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://bkwbdoupnnvojiavzmwm.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrd2Jkb3Vwbm52b2ppYXZ6bXdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc3NDkxNTgsImV4cCI6MTk5MzMyNTE1OH0.Em7TTrYq9XJspaThUUG2ABaVxyTvmu_ghtd1DgG0swA',
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AccountComponent } from './account/account.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
